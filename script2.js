dbbeaa495f84e33936a1428221ec286ce5c811d8
Wisdom Simeon <121321306+Wisdomsimeon@users.noreply.github.com>
2025-01-30 23:02:41 +0100

    script2.js

--- script2.js
+++ script2.js
@@ -24,8 +24,8 @@ camera.position.set(0,1,2);
 const sky = new Sky();
 sky.scale.setScalar( 450000 );
 
-const phi = THREE.MathUtils.degToRad( 90 );
-const theta = THREE.MathUtils.degToRad( 180 );
+const phi = new THREE.MathUtils.degToRad( 90 );
+const theta = new THREE.MathUtils.degToRad( 180 );
 const sunPosition = new THREE.Vector3().setFromSphericalCoords( 1, phi, theta );
 
 sky.material.uniforms.sunPosition.value = sunPosition;
